839aa7e65677c9e7bdbda2004e844d00b63d89e7
Terry Tai <poshboytl@gmail.com>
2016-12-09 17:12:59 +0800

    Move the authentication related functions to a concern.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,30 +1,5 @@
 class Author < ApplicationRecord
+  include Authentication
+
   has_many :posts
-
-  def self.authenticate email, pass
-    author = Author.find_by_email email
-    return author if author && Digest::SHA256.hexdigest(pass + author.salt) == author.hashed_password
-    false
-  end
-
-  def password
-    @password
-  end
-
-  def password=(pass)
-    return unless pass
-    @password = pass
-    generate_password pass
-  end
-
-  private
-
-  def generate_password pass
-    self.salt, self.hashed_password =
-      password_salt, Digest::SHA256.hexdigest(pass + password_salt)
-  end
-
-  def password_salt
-    @password_salt ||= Array.new(10){rand(1024).to_s(36)}.join
-  end
-end
+ end
---
+++ app/models/concerns/authentication.rb
@@ -0,0 +1,44 @@
+module Authentication
+  extend ActiveSupport::Concern
+
+  class_methods do
+
+    def authenticate email, pass
+      author = Author.find_by_email email
+      return author if author && secure_compare(Digest::SHA256.hexdigest(pass + author.salt), author.hashed_password)
+      false
+    end
+
+    def secure_compare(a, b)
+      return false if a.blank? || b.blank? || a.bytesize != b.bytesize
+      l = a.unpack "C#{a.bytesize}"
+
+      res = 0
+      b.each_byte { |byte| res |= byte ^ l.shift }
+      res == 0
+    end
+
+  end
+
+  def password
+    @password
+  end
+
+  def password=(pass)
+    return unless pass
+    @password = pass
+    generate_password pass
+  end
+
+  private
+
+  def password_salt
+    @password_salt ||= Array.new(10){rand(1024).to_s(36)}.join
+  end
+
+  def generate_password pass
+    self.salt, self.hashed_password =
+      password_salt, Digest::SHA256.hexdigest(pass + password_salt)
+  end
+
+end
