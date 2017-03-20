4d7e8cbe6a7f741821f6f30ca14a0db8dbbf8903
Terry Tai <poshboytl@gmail.com>
2017-03-20 21:21:05 +0800

    Should only list published posts in archinved.

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,7 +1,7 @@
 class ArchivesController < ApplicationController
 
   def show
-    @posts = Post.article.order("id DESC").group_by { |post| post.created_at.beginning_of_month }
+    @posts = Post.article.order("id DESC").published.group_by { |post| post.created_at.beginning_of_month }
   end
 
 end
