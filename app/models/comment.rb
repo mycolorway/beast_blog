36f54790dc90b697b7810078286c7f5d6dabb283
shaojunda <shaojunda@gmail.com>
2017-04-04 22:06:22 +0800

    FIX: comment bug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :user, optional: true
 
   def can_delete_by user
-    user.author? || comment.user == user
+    user.author? || self.user == user
   end
 end
